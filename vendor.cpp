dbd64e80bed517f9d30753b777f7f2fbf4380085
Lateefah camacho <lateefah.camacho07@gmail.com>
2025-12-10 09:55:06 -0800

    milestone 1

--- vendor.cpp
+++ vendor.cpp
@@ -2,3 +2,37 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+
+using namespace std;
+
+struct Customer {
+    string name;
+    string drinkOrder;
+};
+
+struct ListNode {
+    Customer data;
+    ListNode* next;
+};
+
+int main()
+{
+    srand(time(0));
+
+    string names[] = {"Alice", "Bob", "Charlie", "Diana", "Ethan",
+        "Fiona", "George", "Hannah", "Ian", "Julia",
+        "Kevin", "Luna", "Mike", "Nora", "Oscar"
+    };
+    
+    string drinks[] = { "Espresso", "Latte", "Cappuccino", "Americano", "Mocha",
+        "Macchiato", "Flat White", "Cold Brew", "Tea", "Hot Chocolate",
+        "Iced Coffee", "Frappe", "Matcha Latte", "Chai Tea", "Affogato"
+    };
+
+
+
+    
+    return 0;
+}
+
+
